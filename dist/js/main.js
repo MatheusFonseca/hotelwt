4e3ea5364a6b3f79deeea0641d916ca599c23b96
Matheus Fonseca <matheusfonsecamorais@gmail.com>
2019-07-20 18:22:49 -0300

    Primeiro Commit

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+$('.showcase__seta').on('click', function(event) {
+  if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+          {
+              scrollTop: $(hash).offset().top
+          },
+          800
+      );
+  }
+});
